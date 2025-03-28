83fb0ca54f871c0c3795a01c90bf16f507325a23
Kamaldevgn <kamaldevgn@gmail.com>
2025-03-28 22:37:28 +0530

    navbar added

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,17 @@
 export default function Header() {
-    return (
-      <header className="bg-blue-600 text-white py-4">
-        <h1 className="text-center text-2xl font-bold">My Portfolio</h1>
-      </header>
-    );
-  }
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Left-aligned heading */}
+        <h1 className="text-xl font-bold">Kamal</h1>
+        
+        {/* Right-aligned links */}
+        <nav>
+          <a href="" className="mx-2 hover:text-gray-400">Home</a>
+          <a href="/about" className="mx-2 hover:text-gray-400">About</a>
+          <a href="/contact" className="mx-2 hover:text-gray-400">Contact</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
