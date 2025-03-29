4b6debc74aea4f46088b0242ff15a6d582751216
Kamaldevgn <kamaldevgn@gmail.com>
2025-03-29 23:03:28 +0530

    some work in the main section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
     <section>
-      <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio!</h1>
-      <p className="text-lg">Explore my projects and learn more about me.</p>
-    </section>
+      <h1 className="text-4xl font-bold mb-4">I'm Kamalpreet Singh</h1>
+      <h2 className="text-3xl font-bold mb-4">About</h2>
+      <p>A frontend engineer, specialize in building dynamic web applications using TypeScript, Next.js, and React. I love using Tailwind CSS to create responsive designs and manage data with PostgreSQL, which keeps everything running smoothly. Im all about writing clean code and keeping up with the latest trends, aiming to deliver high-quality projects that users actually enjoy—without any pesky bugs, of course!</p>
+      <div className="pt-3 flex justify-center">
+        <a href="" className="px-3 text-2xl">Github</a>
+        <a href="" className="px-3 text-2xl">LinkedIn</a>
+        <a href="" className="px-3 text-2xl">Instagram</a>
+      </div>
+          </section>
   );
 }
